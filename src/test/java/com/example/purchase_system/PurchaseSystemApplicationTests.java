8482aee16c81c285512dec6994ebd82c77111622
AMM-wrightreeku <amrexekum@gmail.com>
2026-09-07 03:50:27 +0800

    Initial Spring Boot project

---
+++ src/test/java/com/example/purchase_system/PurchaseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purchase_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
