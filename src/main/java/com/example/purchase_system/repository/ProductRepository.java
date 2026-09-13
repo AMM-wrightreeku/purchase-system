f3ae066780c720640a93f777b446013083cdceab
AMM-PC <amrexekum@gmail.com>
2026-09-13 13:48:02 +0900

    ProductRepositoy find方法 精簡化 邏輯統一化

--- src/main/java/com/example/purchase_system/repository/ProductRepository.java
+++ src/main/java/com/example/purchase_system/repository/ProductRepository.java
@@ -19,48 +19,39 @@ public class ProductRepository {
     }
 
     public Product save(String barcode, String name) {
-
+        // check
         if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("Name cannot be blank.");
+            throw new IllegalArgumentException("Name cannot be blank."); // 空白名字 
         }
-
         if (barcode != null && findByBarcode(barcode).isPresent()) {
-            throw new IllegalArgumentException("Barcode already exists");
+            throw new IllegalArgumentException("Barcode already exists"); // 條碼已經存在
         }
-        // 呼叫 util 直接找 ID
-        int id = IdGenerator.getNextId(products.keySet());
-
+        // main motion
+        int id = IdGenerator.getNextId(products.keySet()); // 呼叫 util 直接找 ID
         Product product = new Product(id, barcode, name);
-
         products.put(id, product);
-
         return product;
     }
-
+    // find
     public Optional<Product> findById(int id) {
         return Optional.ofNullable(products.get(id));
     }
-
     public Optional<Product> findByBarcode(String barcode) {
+        if(barcode == null || barcode.isBlank()) return Optional.empty();
 
         for (Product product : products.values()) {
-            if (product.getBarcode() != null
-                    && product.getBarcode().equals(barcode)) {
+            if (barcode.equals(product.getBarcode())) {
                 return Optional.of(product);
             }
         }
-
+        
         return Optional.empty();
     }
-
     public List<Product> findByName(String name) {
-
         List<Product> result = new ArrayList<>();
-
-        if (name == null || name.isBlank()) {
-            return result;
-        }
-
+        
+        if (name == null || name.isBlank()) return result;
+        
         for (Product product : products.values()) {
             if (product.getName().contains(name)) {
                 result.add(product);
